8ab84ca69c60e2e6b89f773a8838ccf67c8855c3
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-03-03 15:52:33 +0100

    Adjust routes to mongodb

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
+const mongoose = require('mongoose')
+const User = require('./models/User')
+
+mongoose
+  .connect('mongodb://localhost/lean-coffee-board', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connected to mongodb'))
+  .catch(error => console.error('Could not connect to mongodb', error))
 
 const app = express()
+app.use(express.json())
 
 let users = []
 
-app.use(express.json())
-
-app.get('/api/users', (req, res) => {
-  res.json(users)
+app.get('/api/users', async (req, res) => {
+  //User.find().then(users => res.json(users))
+  res.json(await User.find())
 })
 
-app.post('/api/users', (req, res) => {
-  users.push({ ...req.body, id: uuidv4() })
-  res.json(req.body)
-})
-
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/users/:id', async (req, res) => {
   const { id } = req.params
-  res.json(users.find(user => user.id === id))
+  res.json(await User.findOne({ id }))
+})
+
+app.post('/api/users', async (req, res) => {
+  res.json(await User.create(req.body))
 })
 
 app.delete('/api/users/:id', (req, res) => {
